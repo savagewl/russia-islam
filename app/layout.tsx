350c24d17675534a1a2bb2a650a6ccb7e10f1621
Acrelis <acrelis@MacBook-Air-Acrelis.local>
2026-06-29 17:33:53 +0300

    Yandex Metrix

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=110251281','ym');
+          ym(110251281,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+        `}} />
+        <noscript dangerouslySetInnerHTML={{ __html: `<div><img src="https://mc.yandex.ru/watch/110251281" style="position:absolute;left:-9999px;" alt="" /></div>` }} />
+      </head>
       <body>
         <div className="site-container">
           <ClientLayout>
